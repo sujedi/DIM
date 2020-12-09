4203b4fb88c0ec8d9074abd6866ac0fbbb6266e1
sujedi <55679368+sujedi@users.noreply.github.com>
2020-12-10 08:22:31 +0900

    Update setup.py
    I installed cortex from 'https://github.com/rdevon/cortex' and its version is 0.11.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='cortex_DIM',
       author_email='erroneus@gmail.com',
       packages=['cortex_DIM', 'cortex_DIM.configs', 'cortex_DIM.functions', 'cortex_DIM.nn_modules',
                 'cortex_DIM.models', 'cortex_DIM.evaluation_models'],
-      install_requires=['cortex==0.13a0'],
+      install_requires=['cortex==0.11'],
       zip_safe=False)
